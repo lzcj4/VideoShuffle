354a07e177c346b1f7e8d143fd81de9ad7e0726c
lzcj4 <lzcj4@163.com>
2016-02-16 17:50:18 +0800

    add RXJava

--- app/src/main/java/com/nero/videoshuffle/view/TestView.java
+++ app/src/main/java/com/nero/videoshuffle/view/TestView.java
@@ -6,6 +6,12 @@ import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
+import rx.Observable;
+import rx.Scheduler;
+import rx.functions.Action0;
+import rx.functions.Action1;
+import rx.schedulers.Schedulers;
+
 /**
  * Created by nlang on 2/15/2016.
  */
@@ -27,14 +33,23 @@ public class TestView extends View {
     @Override
     public boolean dispatchTouchEvent(MotionEvent event) {
         boolean result = super.dispatchTouchEvent(event);
-        Log.i(TAG, String.format("/*** dispatchTouchEvent=%s  ,action=%s ****/", result,event.getAction()));
+        Log.i(TAG, String.format("/*** dispatchTouchEvent=%s  ,action=%s ****/", result, event.getAction()));
         return result;
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         boolean result = super.onTouchEvent(event);
-        Log.i(TAG, String.format("/+++ onTouchEvent=%s  ,action=%s ++++/", result,event.getAction()));
+        Log.i(TAG, String.format("/+++ onTouchEvent=%s  ,action=%s ++++/", result, event.getAction()));
+
+        Observable<String> observable = Observable.just("aa").subscribeOn(Schedulers.newThread());
+        observable.subscribe(new Action1<String>() {
+                                 @Override
+                                 public void call(String s) {
+                                     Log.d("RX",s);
+                                 }
+                             }
+        );
         return result;
     }
 }
