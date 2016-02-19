abc83dd0ec40270cf3b023c0881ec361c721fe4c
lzcj4 <lzcj4@163.com>
2016-02-19 17:19:15 +0800

    update unit test

--- app/src/androidTest/java/com/nero/videoshuffle/MyProxyTest.java
+++ app/src/androidTest/java/com/nero/videoshuffle/MyProxyTest.java
@@ -1,7 +1,7 @@
 package com.nero.videoshuffle;
 
 import android.support.test.runner.AndroidJUnit4;
-import android.test.suitebuilder.annotation.SmallTest;
+import android.test.suitebuilder.annotation.LargeTest;
 import android.util.Log;
 
 import com.nero.videoshuffle.util.MyProxy;
@@ -9,8 +9,6 @@ import com.nero.videoshuffle.util.MyProxy;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import static org.junit.Assert.*;
 
 import java.io.BufferedInputStream;
 import java.io.IOException;
@@ -20,12 +18,19 @@ import java.net.Proxy;
 import java.net.URL;
 import java.net.URLConnection;
 
+import rx.Observable;
+import rx.Subscriber;
+import rx.functions.Action1;
+import rx.schedulers.Schedulers;
+
+import static org.junit.Assert.assertNotNull;
+
 /**
  * Created by nlang on 2/19/2016.
  */
 
 @RunWith(AndroidJUnit4.class)
-@SmallTest
+@LargeTest
 public class MyProxyTest {
     MyProxy myProxy;
 
@@ -38,44 +43,29 @@ public class MyProxyTest {
     @Test
     public void testProxy() {
 
-//        Observable<String> observable=Observable.just("http://www.163.com")
-//                .observeOn(Schedulers.io())
-//                .subscribeOn(Schedulers.test())
-//                .subscribe(new Action1<String>() {
-//                    @Override
-//                    public void call(String s) {
-//                        Log.i("MyProxy", s);
-//                    }
-//                });
-
-
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    URL url = new URL("http://www.163.com");
-                    URLConnection con = url.openConnection(new Proxy(Proxy.Type.HTTP, new InetSocketAddress(MyProxy.PORT)));
-                    // con.setDoOutput(true);
-                    InputStream inputStream = con.getInputStream();
-                    BufferedInputStream bufferedStream = new BufferedInputStream(inputStream);
-                    byte[] buffer = new byte[1024];
-                    StringBuilder sb = new StringBuilder();
-                    int readLen = 0;
-                    while ((readLen = bufferedStream.read(buffer)) > 0) {
-                        String s = new String(buffer, 0, readLen, "utf-8");
-                        sb.append(s);
-                        Log.i("MyProxy", s);
-                    }
-
-                    String html = sb.toString();
-                    assertNotNull(null);
-                    Log.i("MyProxy", html);
-
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-
+        final String uri = "http://www.163.com";
+        try {
+            URL url = new URL(uri);
+            URLConnection con = url.openConnection(new Proxy(Proxy.Type.HTTP, new InetSocketAddress(MyProxy.PORT)));
+            // con.setDoOutput(true);
+            InputStream inputStream = con.getInputStream();
+            BufferedInputStream bufferedStream = new BufferedInputStream(inputStream);
+            byte[] buffer = new byte[1024];
+            StringBuilder sb = new StringBuilder();
+            int readLen = 0;
+            while ((readLen = bufferedStream.read(buffer)) > 0) {
+                String s = new String(buffer, 0, readLen, "utf-8");
+                sb.append(s);
+                Log.i("MyProxy", s);
             }
-        }).start();
+
+            String html = sb.toString();
+            assertNotNull(html);
+            Log.i("MyProxy", html);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
