f747c583ec44647cef2a5a4c17ecf38f4e8508c7
lzcj4 <lzcj4@163.com>
2016-03-31 17:56:00 +0800

    add animation

--- app/src/main/java/com/nero/videoshuffle/view/Chart.java
+++ app/src/main/java/com/nero/videoshuffle/view/Chart.java
@@ -1,5 +1,6 @@
 package com.nero.videoshuffle.view;
 
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
@@ -40,12 +41,22 @@ public class Chart extends View {
     final int DEFAULTLEGLEN = 200;
 
     @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        startAnimator();
+    }
+
+    @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        drawStart(canvas);
+        if (i++ % 2 == 0) {
+            drawStand(canvas);
+        } else {
+            drawWalk(canvas);
+        }
     }
 
-    private void drawStart(Canvas canvas) {
+    private void drawWalk(Canvas canvas) {
         final int getWidth = getMeasuredWidth();
         final int getPaddingTop = getPaddingTop() == 0 ? DEFAULTPADDING : getPaddingTop();
         final int getPaddingStart = getPaddingLeft() == 0 ? DEFAULTPADDING : getPaddingLeft();
@@ -53,19 +64,22 @@ public class Chart extends View {
         final float cx = (getWidth - getPaddingStart - getPaddingBottom) / 2.0F;
         final float cy = getPaddingTop + DEFAULTRADIUS;
 
+        //Head
         canvas.drawCircle(cx, cy, DEFAULTRADIUS, mPaint);
         final double cosLeft = Math.cos(2 * Math.PI / 360 * 210);
         final double cosRight = Math.cos(2 * Math.PI / 360 * 330);
 
+        //Arm
         float len = (float) cosLeft * DEFAULTARMLEN;
         float cay = cy + DEFAULTRADIUS;
         canvas.drawLine(cx, cay, cx + len, cay + DEFAULTARMLEN, mPaint);
-
-        canvas.drawLine(cx, cay, cx, cay + DEFAULTBODYLEN, mPaint);
-
         len = (float) cosRight * DEFAULTARMLEN;
         canvas.drawLine(cx, cay, cx + len, cay + DEFAULTARMLEN, mPaint);
 
+        //Body
+        canvas.drawLine(cx, cay, cx, cay + DEFAULTBODYLEN, mPaint);
+
+        //Leg
         cay = cay + DEFAULTBODYLEN;
         len = (float) cosLeft * DEFAULTLEGLEN;
         canvas.drawLine(cx, cay, cx + len, cay + DEFAULTLEGLEN, mPaint);
@@ -74,7 +88,7 @@ public class Chart extends View {
         canvas.drawLine(cx, cay, cx + len, cay + DEFAULTLEGLEN, mPaint);
     }
 
-    private void drawEnd(Canvas canvas) {
+    private void drawStand(Canvas canvas) {
         final int getWidth = getMeasuredWidth();
         final int getPaddingTop = getPaddingTop() == 0 ? DEFAULTPADDING : getPaddingTop();
         final int getPaddingStart = getPaddingLeft() == 0 ? DEFAULTPADDING : getPaddingLeft();
@@ -82,25 +96,56 @@ public class Chart extends View {
         final float cx = (getWidth - getPaddingStart - getPaddingBottom) / 2.0F;
         final float cy = getPaddingTop + DEFAULTRADIUS;
 
+        //head
         canvas.drawCircle(cx, cy, DEFAULTRADIUS, mPaint);
+
+        //Arm
         final double cosLeft = Math.cos(2 * Math.PI / 360 * 210);
         final double cosRight = Math.cos(2 * Math.PI / 360 * 330);
 
         float len = (float) cosLeft * DEFAULTARMLEN;
         float cay = cy + DEFAULTRADIUS;
         canvas.drawLine(cx, cay, cx + len, cay + DEFAULTARMLEN, mPaint);
-
-        canvas.drawLine(cx, cay, cx, cay + DEFAULTBODYLEN, mPaint);
-
         len = (float) cosRight * DEFAULTARMLEN;
         canvas.drawLine(cx, cay, cx + len, cay + DEFAULTARMLEN, mPaint);
 
-        cay = cay + DEFAULTBODYLEN;
-        len = (float) cosLeft * DEFAULTLEGLEN;
-        canvas.drawLine(cx, cay, cx + len, cay + DEFAULTLEGLEN, mPaint);
+        //Body
+        canvas.drawLine(cx, cay, cx, cay + DEFAULTBODYLEN, mPaint);
 
-        len = (float) cosRight * DEFAULTLEGLEN;
-        canvas.drawLine(cx, cay, cx + len, cay + DEFAULTLEGLEN, mPaint);
+        //Leg
+        final int bodyWidth = 50;
+        cay = cay + DEFAULTBODYLEN;
+        canvas.drawLine(cx - bodyWidth, cay, cx + bodyWidth, cay, mPaint);
+
+        canvas.drawLine(cx - bodyWidth, cay, cx - bodyWidth, cay + DEFAULTLEGLEN, mPaint);
+
+        canvas.drawLine(cx + bodyWidth, cay, cx + bodyWidth, cay + DEFAULTLEGLEN, mPaint);
+    }
+
+    private void drawCircle() {
+
+    }
+
+    int i = 0;
+    boolean isStartAnimator = false;
+
+    private void startAnimator() {
+        if (isStartAnimator) {
+            return;
+        }
+        ValueAnimator va = ValueAnimator.ofFloat(1.0f, 2.0f);
+        va.addUpdateListener((a) -> {
+            float value = (float) a.getAnimatedValue();
+            if (value > 1.9f) {
+                postInvalidate();
+            }
+        });
+        va.setDuration(500);
+        va.setRepeatMode(ValueAnimator.RESTART);
+        va.setRepeatCount(ValueAnimator.INFINITE);
+        va.setTarget(this);
+        va.start();
+        isStartAnimator = true;
     }
 
     private int dp2pixel(float dp) {
